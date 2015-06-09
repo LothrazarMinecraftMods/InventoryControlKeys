ed763b4fc07fd74256ddefa645ad75f6cb03f369
sam bassett <samson.bassett@gmail.com>
2015-06-09 09:43:07 -0700

    macros standalone has only macros. new mods for block control and inventory control

---
+++ src/main/java/com/lothrazar/samsinvcontrol/ModInventory.java
@@ -0,0 +1,189 @@
+package com.lothrazar.samsinvcontrol;
+
+import java.util.ArrayList; 
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.Logger;    
+
+import com.lothrazar.samsinvcontrol.proxy.ClientProxy;
+import com.lothrazar.samsinvcontrol.proxy.CommonProxy;
+import com.lothrazar.samsinvcontrol.proxy.MessageKeyPressed;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.IChatComponent;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
+  
+@Mod(modid = ModInventory.MODID, version = ModInventory.VERSION,	name = ModInventory.NAME, useMetadata = true )  
+public class ModInventory
+{
+	public static final String MODID = "samsinvcontrol"; 
+	public static final String TEXTURE_LOCATION = MODID + ":";
+	public static final String VERSION = "1.8-1.0.0";
+	public static final String NAME = "Sam's Keys and Macros";
+	@Instance(value = MODID)
+	public static ModInventory instance;
+	@SidedProxy(clientSide="com.lothrazar.samskeys.samsinvcontrol.ClientProxy", serverSide="com.lothrazar.samsinvcontrol.proxy.CommonProxy")
+	public static CommonProxy proxy;   
+	public static Logger logger; 
+	public static SimpleNetworkWrapper network;  
+  
+	@EventHandler
+	public void onPreInit(FMLPreInitializationEvent event)
+	{ 
+		logger = event.getModLog();  
+	
+    	network = NetworkRegistry.INSTANCE.newSimpleChannel( MODID );     	
+    	
+    	network.registerMessage(MessageKeyPressed.class, MessageKeyPressed.class, MessageKeyPressed.ID, Side.SERVER);
+
+		this.registerEventHandlers();  
+	}
+        
+	@EventHandler
+	public void onInit(FMLInitializationEvent event)
+	{      
+		proxy.registerRenderers();
+	}
+
+	private void registerEventHandlers() 
+	{ 
+		FMLCommonHandler.instance().bus().register(instance); 
+		MinecraftForge.EVENT_BUS.register(instance);   
+	}
+	
+	public static void incrementPlayerIntegerNBT(EntityPlayer player, String prop, int inc)
+	{
+		int prev = player.getEntityData().getInteger(prop);
+		prev += inc; 
+		player.getEntityData().setInteger(prop, prev);
+	}
+   
+	@SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) 
+    {   
+        if(ClientProxy.keyShiftUp.isPressed() )
+        { 	     
+        	 ModInventory.network.sendToServer( new MessageKeyPressed(ClientProxy.keyShiftUp.getKeyCode()));  
+        }        
+        else if(ClientProxy.keyShiftDown.isPressed() )
+        { 	      
+        	 ModInventory.network.sendToServer( new MessageKeyPressed(ClientProxy.keyShiftDown.getKeyCode()));  
+        }      
+        else if(ClientProxy.keyBarDown.isPressed() )
+        { 	      
+        	 ModInventory.network.sendToServer( new MessageKeyPressed(ClientProxy.keyBarDown.getKeyCode()));  
+        }  
+        else if(ClientProxy.keyBarUp.isPressed() )
+        { 	      
+        	 ModInventory.network.sendToServer( new MessageKeyPressed(ClientProxy.keyBarUp.getKeyCode()));  
+        }   
+
+    } 
+	
+	public static void playSoundAt(World world,BlockPos pos, String sound)
+	{ 
+		world.playSound(pos.getX(), pos.getY(), pos.getZ(), sound, 1.0F, 1.0F, false);
+	}
+	
+	public static ArrayList<Block> getBlockListFromCSV(String csv)
+	{
+		 ArrayList<Block> blocks = new ArrayList<Block>();
+		 String[] ids = csv.split(",");  
+		 Block b; 
+		 
+		 for(String id : ids)
+		 {
+			 b = Block.getBlockFromName(id);
+			 
+			 if(b == null)
+			 {
+				 ModInventory.logger.log(Level.WARN, "getBlockListFromCSV : Block not found : "+id);
+			 }
+			 else 
+			 {
+				 blocks.add(b);
+			 }
+		 } 
+		 
+		 return blocks;
+	}
+	public static void playSoundAt(Entity player, String sound)
+	{ 
+		player.worldObj.playSoundAtEntity(player, sound, 1.0F, 1.0F);
+	}
+ 
+	public static String lang(String name)
+	{
+		return StatCollector.translateToLocal(name);
+	}
+
+	public static void addChatMessage(String string) 
+	{ 
+		addChatMessage(new ChatComponentTranslation(string)); 
+	}
+	public static void addChatMessage(IChatComponent string) 
+	{ 
+		 Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(string); 
+	}
+	public static void addChatMessage(EntityPlayer player,String string) 
+	{ 
+		player.addChatMessage(new ChatComponentTranslation(string));
+	}
+	
+	public static EnumFacing getPlayerFacing(EntityPlayer player) 
+	{
+    	int yaw = (int)player.rotationYaw;
+
+        if (yaw<0)              //due to the yaw running a -360 to positive 360
+           yaw+=360;    //not sure why it's that way
+
+        yaw += 22;     //centers coordinates you may want to drop this line
+        yaw %= 360;  //and this one if you want a strict interpretation of the zones
+
+        int facing = yaw/45;   //  360degrees divided by 45 == 8 zones
+       
+		return EnumFacing.getHorizontal( facing/2 );
+	}
+	public static void spawnParticle(World world, EnumParticleTypes type, BlockPos pos)
+	{
+		spawnParticle(world,type,pos.getX(),pos.getY(),pos.getZ());
+	}
+
+	public static void spawnParticle(World world, EnumParticleTypes type, double x, double y, double z)
+	{ 
+		//http://www.minecraftforge.net/forum/index.php?topic=9744.0
+		for(int countparticles = 0; countparticles <= 10; ++countparticles)
+		{
+			world.spawnParticle(type, x + (world.rand.nextDouble() - 0.5D) * (double)0.8, y + world.rand.nextDouble() * (double)1.5 - (double)0.1, z + (world.rand.nextDouble() - 0.5D) * (double)0.8, 0.0D, 0.0D, 0.0D);
+		} 
+    }
+	public static void execute(EntityPlayer player, String cmd)
+	{
+		MinecraftServer.getServer().getCommandManager().executeCommand(player, cmd);
+	}
+
+}
---
+++ src/main/java/com/lothrazar/samsinvcontrol/proxy/ClientProxy.java
@@ -0,0 +1,46 @@
+package com.lothrazar.samsinvcontrol.proxy;
+
+import org.lwjgl.input.Keyboard;   
+
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.client.settings.KeyBinding;
+
+public class ClientProxy extends CommonProxy 
+{  
+	public static KeyBinding keyShiftUp;
+	public static KeyBinding keyShiftDown; 
+	public static KeyBinding keyBarUp;
+	public static KeyBinding keyBarDown; 
+	public static final String keyUpName = "key.columnshiftup";
+	public static final String keyDownName = "key.columnshiftdown"; 
+	public static final String keyBarUpName = "key.columnbarup";
+	public static final String keyBarDownName = "key.columnbardown"; 
+ 
+	public static final String keyCategoryInventory = "key.categories.inventorycontrol";
+	
+    @Override
+    public void registerRenderers() 
+    {  
+    	registerKeyBindings();  
+    }
+  
+	private void registerKeyBindings() 
+	{
+		keyShiftUp = new KeyBinding(keyUpName, Keyboard.KEY_Y, keyCategoryInventory);
+        ClientRegistry.registerKeyBinding(ClientProxy.keyShiftUp);
+    
+		keyShiftDown = new KeyBinding(keyDownName, Keyboard.KEY_H, keyCategoryInventory); 
+        ClientRegistry.registerKeyBinding(ClientProxy.keyShiftDown); 
+
+        keyBarUp = new KeyBinding(keyBarUpName, Keyboard.KEY_U, keyCategoryInventory);
+        ClientRegistry.registerKeyBinding(ClientProxy.keyBarUp);
+         
+        keyBarDown = new KeyBinding(keyBarDownName, Keyboard.KEY_J, keyCategoryInventory); 
+        ClientRegistry.registerKeyBinding(ClientProxy.keyBarDown);
+
+        
+         
+	} 
+
+}
